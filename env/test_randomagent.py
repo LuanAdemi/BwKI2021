68a0bbf97e39e0804d8561b77dd9c41e28ac382d
Luan Ademi <26838000+LuanAdemi@users.noreply.github.com>
2021-06-20 18:29:01 +0200

    Random agent test

--- env/test_randomagent.py
+++ env/test_randomagent.py
@@ -1,6 +1,12 @@
 from maumau import MauMauEnv
+import random
 
 env = MauMauEnv(5, 4)
 
 obs, reward, done = env.reset()
-print(obs, env.currentPlayer.getActionMask(env.playStack, binary=False))
+turns = 0
+while not done:
+    action = random.choice(env.currentPlayer.getActionMask(env.playStack, binary=False))
+    obs, reward, done = env.step(action)
+    turns += 1
+print(f"winner: {env.currentPlayer}, iters: {turns}")
