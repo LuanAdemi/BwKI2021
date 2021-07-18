8edea2307a613346d684f79c0272256bb60be5a0
Davanchama <davanchama@gmail.com>
2021-07-18 17:25:16 +0200

    finished move representation, promotions untested (WARNING: REVIEWING THE UPLOADED CODE MIGHT RESULT IN SERIOUS HEALTH ISSUES. CODER DESCRETION IS ADVISED!)

--- env/utils_chess.py
+++ env/utils_chess.py
@@ -50,12 +50,12 @@ class Player:
             assert action in CARDS, "The provided action is invalid"
             self.playCard(action, playStack)
 
-    # returns a bool array containing all legal moves
+    # returns a bool array containing all moves in the list
     # WARNING: If you wish to understand the mysteries and secrets of this method,
     # you will be forced to draw or something like that idk
-    def getActionMask(self, legal_moves):
-        bMask = np.zeros((8, 8, 73), dtype=int)
-        for move in legal_moves:
+    def getActionMask(self, moves):
+        bMask = np.zeros((8, 8, 76), dtype=int)
+        for move in moves:
             dim1 = ord(move[0])-96 #a->1 and so on
             dim2 = int(move[1])
             file = ord(move[2])-96
@@ -74,7 +74,7 @@ class Player:
             elif dim2 == rank: #moves horicontally
                 steps = dim1-file
                 if steps<0: #moves to right (f. w. p.)
-                    dim3 = steps+(2*7)
+                    dim3 = abs(steps)+(2*7)
                 elif steps>0: #moves to left
                     dim3 = steps+(3*7)
                 else:
@@ -145,7 +145,7 @@ class Player:
                 elif typeOfProm == "r":
                     dim3 = 72+placeOfProm
                 elif typeOfProm == "q":
-                    pass
+                    dim3 = 75+placeOfProm
                 else:
                     print("Invalid promotion type: ", typeOfProm)
                     pass
@@ -156,8 +156,135 @@ class Player:
             dim3-=1
             bMask[dim1, dim2, dim3] = 1
         return bMask
+    
+    def getAction(self, mask):
+        dimensions = np.array(np.where(mask == 1)).T
+        moves = []
+        for move in dimensions: #[0, 0, 0] for a1a2
+            dim1 = move[0] #going to be endFile
+            dim2 = move[1] #going to be endRank
+            dim3 = move[2]
+            dim4 = 0 #potential promotion piece
+            moveStr = ""
+            #startFile = chr(dim1+97)
+            #startRank = str(dim2+1)
+            
+            if(dim3<0 and dim3>73): #i wrote this and then read the doc of match-case :(
+                print("WTF IS THIS SHIT??? HONESTLY WHICH IDIOT DID... oh")
+                pass
+            elif(dim3>=0 and dim3<7): #moves up
+                dim2+=dim3+1
+            elif(dim3>=7 and dim3<14): #moves down
+                dim2-=dim3-6
+            elif(dim3>=14 and dim3<21): #moves right
+                dim1+=dim3-13
+            elif(dim3>=21 and dim3<28): #moves left
+                dim1-=dim3-20
+            elif(dim3>=28 and dim3<35): #moves up right
+                dim1+=dim3-27
+                dim2+=dim3-27
+            elif(dim3>=35 and dim3<42): #moves down right
+                dim1-=dim3-34
+                dim2+=dim3-34
+            elif(dim3>=42 and dim3<49): #moves down left
+                dim1-=dim3-41
+                dim2-=dim3-41
+            elif(dim3>=49 and dim3<56): #moves up left
+                dim1+=dim3-48
+                dim2-=dim3-48
+            #queen movement done
+            elif(dim3==56): #8 knight moves
+                dim2+=2
+                dim1+=1
+            elif(dim3==57):
+                dim2+=1
+                dim1+=2
+            elif(dim3==58):
+                dim2-=1
+                dim1+=2
+            elif(dim3==59):
+                dim2-=2
+                dim1+=1
+            elif(dim3==60):
+                dim2-=2
+                dim1-=1
+            elif(dim3==61):
+                dim2-=1
+                dim1-=2
+            elif(dim3==62):
+                dim2+=1
+                dim1-=2
+            elif(dim3==63):
+                dim2+=2
+                dim1-=1
+            #knight movement done
+            elif(dim3==64): #promotions
+                dim1-=1
+                dim2+=1
+                dim4="n"
+            elif(dim3==65):
+                dim2+=1
+                dim4="n"
+            elif(dim3==66):
+                dim1+=1
+                dim2+=1
+                dim4="n"
+            elif(dim3==67):
+                dim1-=1
+                dim2+=1
+                dim4="b"
+            elif(dim3==68):
+                dim2+=1
+                dim4="b"
+            elif(dim3==69):
+                dim1+=1
+                dim2+=1
+                dim4="b"
+            elif(dim3==70):
+                dim1-=1
+                dim2+=1
+                dim4="r"
+            elif(dim3==71):
+                dim2+=1
+                dim4="r"
+            elif(dim3==72):
+                dim1+=1
+                dim2+=1
+                dim4="r"
+            elif(dim3==73):
+                dim1-=1
+                dim2+=1
+                dim4="q"
+            elif(dim3==74):
+                dim2+=1
+                dim4="q"
+            elif(dim3==75):
+                dim1+=1
+                dim2+=1
+                dim4="q"
+                pass
+            #promos done
+            #finalisation
+            startFile = chr(move[0]+97)
+            startRank = str(move[1]+1)
+            endFile = chr(dim1+97)
+            endRank = str(dim2+1)
+            moveStr+=startFile
+            moveStr+=startRank
+            moveStr+=endFile
+            moveStr+=endRank
+            
+            if(dim4==0):
+                pass
+            else:
+                moveStr+=dim4
+            moves.append(moveStr)
+        return moves
                 
                 
+                
+                
+            
 # loggs object methods decorated with the @logged decorator
 class Logger:
     """
